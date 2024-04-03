41e1ed60677d325363c466727543a564a40bf7d0
byunghak082 <byunghak@dable.io>
2024-04-03 20:53:56 +0900

    [FIX] use single state for spinning cas lock

---
+++ lockfree/cas/spinning_cas.go
@@ -0,0 +1,27 @@
+package spinningcas
+
+import (
+	"runtime"
+	"sync/atomic"
+)
+
+const free int32 = 0
+
+// Spinning Compare And Swap Lock
+type SpinningCas struct {
+	state int32
+}
+
+func newCas() *SpinningCas {
+	return &SpinningCas{}
+}
+
+func (t *SpinningCas) Lock() {
+	for !atomic.CompareAndSwapInt32(&t.state, free, 1) {
+		runtime.Gosched()
+	}
+}
+
+func (t *SpinningCas) Unlock() {
+	atomic.StoreInt32(&t.state, free)
+}
